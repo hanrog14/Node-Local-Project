06f595e2146a82bad51737c6f2d52526566d0259
Hannah Rogers <rogershannah@comcast.net>
2018-09-18 12:52:10 -0400

    node commit

--- bash.js
+++ bash.js
@@ -1,11 +1,12 @@
 process.stdout.write('prompt > ');
-
+const pwd = require('./pwd');
+const ls = require('./ls');
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
 
     if (cmd === 'pwd'){
-    process.stdout.write(`${process.cwd()}`);
-        //process.env.PWD
-    } 
-    process.stdout.write('\nprompt > ');
+        pwd()
+    } else if (cmd === 'ls') {
+        ls();
+    }
 });
